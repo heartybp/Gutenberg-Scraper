9335d9acb77a7a16a89e2f6f95561d25a649dab6
hbp <hearty.bp@gmail.com>
2023-08-04 21:51:33 -0700

    Finished working python script

---
+++ book_scrape.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+from bs4 import BeautifulSoup
+import requests
+
+class BookSearchApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Project Gutenberg Book Search")
+        
+        self.label = tk.Label(root, text="Enter book title:")
+        self.label.pack()
+
+        self.entry = tk.Entry(root)
+        self.entry.pack()
+
+        self.search_button = tk.Button(root, text="Search", command=self.search_books)
+        self.search_button.pack()
+
+    def search_books(self):
+        search_term = self.entry.get()
+        if search_term:
+            self.display_results(search_term)
+        else:
+            messagebox.showinfo("Error", "Please enter a search term.")
+
+    def display_results(self, search_term):
+        base_url = "https://www.gutenberg.org/ebooks/search/?query="
+
+        # construct the search URL
+        search_url = base_url + search_term
+
+        # send GET request to search URL
+        response = requests.get(search_url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # find book titles and display them
+        book_titles = soup.find_all("span", class_="title")
+        if book_titles:
+            messagebox.showinfo("Search Results", "\n".join(title.get_text() for title in book_titles))
+        else:
+            messagebox.showinfo("No Results", "No books found for the search term.")
+
+def main():
+    root = tk.Tk()
+    app = BookSearchApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
